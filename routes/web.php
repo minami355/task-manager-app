565930941b8db70ad35be66cc7014455f197b967
minami <rouet76apt.56@gmail.com>
2026-07-12 21:58:28 +0900

    feat: タスクCRUD実装 #5

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,18 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-//認証が必要なルート
+// 認証が必要なルート
 Route::middleware('auth')->group(function () {
-    //カテゴリーのCRUDルート （仮ルートから置き換え）
+    // カテゴリーのCRUDルート
     Route::resource('categories', CategoryController::class);
 
-    //タスクの仮ルート （次のセクションで本実装に置き換え）
-    Route::get('/tasks', fn() => 'タスク一覧（準備中）')->name('tasks.index');
+    // タスクのCRUDルート（仮ルートから置き換え）
+    Route::resource('tasks', TaskController::class);
 });
